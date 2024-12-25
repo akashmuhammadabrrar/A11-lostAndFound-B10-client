69c9fe7100fddc88c3541bf3c5f6d63958e55c5f
AkashMAbrrar <madborakash48@gmail.com>
2024-12-25 13:35:30 +0600

    this commit for all recovered data get

--- src/Pages/Recoverd/AllRecovered.jsx
+++ src/Pages/Recoverd/AllRecovered.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useAuth from "../../hooks/useAuth";
 
 const AllRecovered = () => {
+  const { user } = useAuth();
+  const [recoveredItems, setRecoveredItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/item-submitted?email=${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setRecoveredItems(data);
+      });
+  }, [user?.email]);
+
   return (
     <div className="mt-20">
-      <h2>All Recovered Items</h2>
+      <h2>All Recovered Items:{recoveredItems.length}</h2>
     </div>
   );
 };
